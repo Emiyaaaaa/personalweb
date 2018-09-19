4056e9aa0fa23e100877fca914cd1408823e7b99
Emiyaaaaa <2914034404@qq.com>
2018-09-19 18:24:18 +0800

    添加自定义field WeatherField;修改xadmin Flatly主题

--- apps/diary/adminx.py
+++ apps/diary/adminx.py
@@ -20,17 +20,17 @@ class GlobalSettings(object):
 
 class Diaryadmin(object):
     # 设置列表显示字段
-    list_display = ['content', 'date','is_display']
+    list_display = ['content', 'date','weather','is_display']
     # 设置列表查询字段
     search_field = ['content','date','is_display']
     # 设置字段可以直接在列表页修改
     list_editable = ['is_display']
     # 设置详情页面不显示某个字段
-    exclude = ['username','like_num','is_display']
+    exclude = ['username','like_num','is_display','username','date','weather','author']
     # 设置默认排序
     # ordering = ['-click_nums']
     # 设置详情页面只读显示
-    # readonly_fields = ['content']
+    # readonly_fields = ['weather']
     # 自动刷新列表页面（秒数）
     # refresh_times = [3, 5]
 
--- apps/diary/models.py
+++ apps/diary/models.py
@@ -1,23 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 from django.db import models
 from datetime import datetime
+import urllib.request
+import json
+import gzip
+
+class WeatherField(models.CharField):
+    def get_weather(self,city = '榆次'):
+        url = 'http://wthrcdn.etouch.cn/weather_mini?city=' + urllib.parse.quote(city)
+        weather_data = urllib.request.urlopen(url).read()
+        weather_data = gzip.decompress(weather_data).decode('utf-8')
+        weather_dict = json.loads(weather_data)
+        forecast = weather_dict.get('data').get('forecast')
+        weather = forecast[0].get('type')
+        return weather
+
+    def get_prep_value(self, value):
+        return self.get_weather()
 
 
 class Diary(models.Model):
     text_id = models.AutoField(primary_key = True)
     username = models.CharField(max_length=50, default='Emiya')
     content = models.TextField(verbose_name=u'正文')
-    # short_content = models.TextField(verbose_name=u'缩略正文',null=True)
+    weather = WeatherField(max_length=100,verbose_name='天气',default='')
     author = models.CharField(verbose_name=u'作者', max_length=100,default='Emiya')
     date = models.DateTimeField(verbose_name=u'日期',default= datetime.now)
-    # weather = models.CharField(verbose_name=u'天气',max_length=20,null=True)
     like_num = models.IntegerField(verbose_name=u'点赞数',default='0')
     created_at = models.DateTimeField(verbose_name=u'创建时间', auto_now_add=True, null=False)
     update_at = models.DateTimeField(verbose_name=u'更新时间', auto_now=True, null=False)
     is_display = models.IntegerField(verbose_name='展示', choices=((0, '显示'), (1, '隐藏')), default=0)
 
     class Meta:
-        verbose_name = u"日记"
+        verbose_name = u"动态"
         verbose_name_plural = verbose_name
 
     def __str__(self):
-        return self.username
+        return self.content

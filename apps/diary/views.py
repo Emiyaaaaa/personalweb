9a351e20664b0c8942a8b16d0e981966517c8b0d
Emiyaaaaa <2914034404@qq.com>
2018-09-21 13:15:30 +0800

    更新天气

--- apps/diary/views.py
+++ apps/diary/views.py
@@ -5,13 +5,34 @@ from .models import Diary
 class DiaryView(View):
     def get(self,request):
         diary_info = []
+        week_dict = {
+            'Mon':'星期一',
+            'Tues':'星期二',
+            'Wed':'星期三',
+            'Thur':'星期四',
+            'Fri':'星期五',
+            'Sat':'星期六',
+            'Sun':'星期日'
+        }
         all_diary = Diary.objects.all().order_by('-text_id')
+        i = 0
         for diary in all_diary:
-            if diary.text_id <= 10:
-                diary_info.append({'content':diary.content,
-                                    'date':diary.date
-                                    })
-            else:
+            date = diary.date
+            year = date.strftime('%Y')
+            month = date.strftime('%m')
+            day = date.strftime('%d')
+            week = date.strftime('%a')
+            cn_week = week_dict[week]
+            try:
+                weather = diary.weather
+            except:
+                weather = ''
+            diary_info.append({'content':diary.content,
+                                'date':year + '年' + month + '月' + day + '日' + ' ' + cn_week + ' ' + weather
+                                })
+            print(diary.date)
+            i = i + 1
+            if i >= 20:
                 break
 
 

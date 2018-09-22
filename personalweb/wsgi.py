7b2992771f467430c32317e864b4965741bea1b6
Emiyaaaaa <2914034404@qq.com>
2018-09-23 01:54:33 +0800

    成功在服务器上运行，纪念一下hhh

--- personalweb/wsgi.py
+++ personalweb/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('C:/personalweb')
+sys.path.append('C:/personalweb/personalweb')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "personalweb.settings")
 

1badbbf22f4e19687e20634e527ef930bd6c6570
Emiyaaaaa <2914034404@qq.com>
2018-09-25 17:30:25 +0800

    update

--- apps/changeLog/adminx.py
+++ apps/changeLog/adminx.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import xadmin
-from .models import ChangeLog,Version
+from .models import ChangeLog,Version,SmallVersion
 
 class VersionAdmin(object):
-    list_display = ['version','update_at']
+    list_display = ['version','Introduction','update_at']
+    exclude = ['update_at','created_at']
+
+
+class SmallVersionAdmin(object):
+    list_display = ['version','small_version','update_at']
     exclude = ['update_at','created_at']
 
 class ChangeLogAdmin(object):
-    list_display = ['version','small_version','update_at']
-    search_field = ['version']
+    list_display = ['small_version','Introduction','update_at']
     exclude = ['update_at','created_at']
 
 xadmin.site.register(Version,VersionAdmin)
+xadmin.site.register(SmallVersion,SmallVersionAdmin)
 xadmin.site.register(ChangeLog,ChangeLogAdmin)

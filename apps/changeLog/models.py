e9bddf213676f1bfddb44accbcfda0a6ecd41216
Emiyaaaaa <2914034404@qq.com>
2018-09-25 17:22:57 +0800

    update

--- apps/changeLog/models.py
+++ apps/changeLog/models.py
@@ -14,9 +14,22 @@ class Version(models.Model):
         return self.version
 
 
-class ChangeLog(models.Model):
+class SmallVersion(models.Model):
     version = models.ForeignKey(Version,on_delete=models.DO_NOTHING)
     small_version = models.CharField(verbose_name='小版本号',max_length=30)
+    created_at = models.DateTimeField(verbose_name=u'创建时间', auto_now_add=True, null=False)
+    update_at = models.DateTimeField(verbose_name=u'更新时间', auto_now=True, null=False)
+
+    class Meta:
+        verbose_name = u"小版本"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.version
+
+
+class ChangeLog(models.Model):
+    version = models.ForeignKey(SmallVersion,on_delete=models.DO_NOTHING)
     Introduction = models.TextField(verbose_name='更新日志')
     created_at = models.DateTimeField(verbose_name=u'创建时间', auto_now_add=True, null=False)
     update_at = models.DateTimeField(verbose_name=u'更新时间', auto_now=True, null=False)

cd1a1afea8cff18fd35c3f1a3c3b53374e59fab3
Emiyaaaaa <2914034404@qq.com>
2018-09-28 11:20:38 +0800

    update

---
+++ git_pull.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Author  : Li Haozheng
+# @Time    : 2018/9/28 11:15
+from git import Repo
+repoPath = r'E:\python\personalweb'
+repo = Repo(repoPath)
+repo.git.checkout('.')
+repo.git.clean('-df')

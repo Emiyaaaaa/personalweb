888b5ace44ae634ea06b7eba468f36b9faf6a899
Emiyaaaaa <2914034404@qq.com>
2018-09-28 18:39:46 +0800

    update

--- personalweb/settings.py
+++ personalweb/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'personalweb.wsgi.application'
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),os.path.join(BASE_DIR, 'media')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'media')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'

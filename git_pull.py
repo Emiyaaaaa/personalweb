c380c6430de6555163998d2a819a43d2cb601734
Emiyaaaaa <2914034404@qq.com>
2018-09-28 18:20:47 +0800

    update

--- git_pull.py
+++ git_pull.py
@@ -5,10 +5,15 @@
 import os
 from  datetime import datetime
 import time
+
+i = 0
 try:
     while True:
         os.system('git pull origin master')
         print(str(datetime.now())+'\n')
         time.sleep(120)
+        print(i)
+        i = i + 1
 except BaseException as e:
     print(str(datetime.now()) + ' ' + str(e))
+    os._exit(1)

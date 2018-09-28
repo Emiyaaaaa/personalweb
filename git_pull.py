0d0bae97e209e71d7a266c5906968372fd3dafd7
Emiyaaaaa <2914034404@qq.com>
2018-09-28 11:57:13 +0800

    update

--- git_pull.py
+++ git_pull.py
@@ -2,9 +2,13 @@
 # -*- coding: utf-8 -*-
 # @Author  : Li Haozheng
 # @Time    : 2018/9/28 11:15
-from git import Repo
-repoPath = r'C:\personalweb'
-repo = Repo(repoPath)
-repo.git.checkout('.')
-
-repo.git.clean('-df')
+import os
+from  datetime import datetime
+import time
+try:
+    while True:
+        os.system('git pull origin master')
+        print('\n'+str(datetime.now()))
+        time.sleep(120)
+except BaseException as e:
+    print(str(datetime.now()) + ' ' + str(e))

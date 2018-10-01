72e75d14c1be7efc0f9dc63da8db8f79b0f848e1
Emiyaaaaa <2914034404@qq.com>
2018-10-01 19:38:09 +0800

    update

--- tools/git_pull.py
+++ tools/git_pull.py
@@ -10,11 +10,11 @@ i = 0
 try:
     while True:
         output = os.popen('git pull origin master')
-        if output.read() ！= 'Already up to date.\n'：
-        	os.system('net stop apache24')
-        	os.system('net start apache24')
+        if output.read() != 'Already up to date.\n':
+        	os.system('net stop Apache2.4')
+        	os.system('net start Apache2.4')
         print(str(datetime.now())+'\n')
-        time.sleep(120)
+        time.sleep(60)
 except BaseException as e:
     print(str(datetime.now()) + ' ' + str(e))
     os._exit(1)

d6f6afb9d769cec1af8adb0e03863828b901a13a
Emiyaaaaa <2914034404@qq.com>
2018-10-06 16:45:39 +0800

    前端更新

--- static/js/flashy.js
+++ static/js/flashy.js
@@ -1,3 +1,54 @@
 $(document).ready(function() {
-	$('.demo').flashy();
-}
+	$('.window').flashy({
+ 
+	    // image, inline, ajax, iframe, video
+	    type: 'inline',
+	   
+	    // show title
+	    title: true,
+	   
+	    // can be any CSS valid unit - px, %, and etc
+	    width: null,
+	    height: null,
+	   
+	    // enable/disable gallery mode
+	    gallery: true, 
+	   
+	    // enable/disable infinite loop
+	    galleryLoop: true, 
+	   
+	    // show item counter
+	    galleryCounter: true, 
+	   
+	    // show prev and next navigation controls
+	    galleryPrevNext: true, 
+	   
+	    // message used in the item counter. If empty, no counter will be displayed
+	    galleryCounterMessage: '[index] / [total]', 
+	   
+	    // enable/disable swipe on desktop
+	    gallerySwipeDesktop: true, 
+	   
+	    // enable/disable swipe on mobile devices
+	    gallerySwipeMobile: true, 
+	   
+	    // set swipe threshold in px
+	    gallerySwipeThreshold: 100, 
+	   
+	    // enable/disable keyboard navigation with arrows and close with ESC
+	    keyboard: true, 
+	   
+	    // close lightbox via the close button or overlay click
+	    overlayClose: false, 
+	   
+	    // additional css classes for customization
+	    themeClass: null, 
+	     
+	    // enable/Disable video autoplay
+	    videoAutoPlay: false, 
+	   
+	    // error message displayed when a content fails to load
+	    loadError: 'Sorry, an error occured while loading the content...'
+ 
+	});
+});

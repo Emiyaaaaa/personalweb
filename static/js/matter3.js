dce01514f6388604a98b4491e188bdcf9cd94630
Emiyaaaaa <2914034404@qq.com>
2019-01-13 19:02:47 +0800

    Update matter3.js

--- static/js/matter3.js
+++ static/js/matter3.js
@@ -32,29 +32,29 @@
     if (!this.parentNode.classList.contains("active")) {
       for (var i = 0; i < links.length; i++) {
         if (links[i].parentNode.classList.contains("active")) {
-          links[i].parentNode.classList.remove("active")
+          links[i].parentNode.classList.remove("active");
         }
         if (i != cn){
-          links[i].style.opacity = "0.25"
+          links[i].style.opacity = "0.25";
         }
       }
 
-      this.parentNode.classList.add("active")
-      this.style.opacity = "1"
+      this.parentNode.classList.add("active");
+      this.style.opacity = "1";
 
-      var width = this.getBoundingClientRect().width
-      var height = this.getBoundingClientRect().height
-      var left = this.getBoundingClientRect().left + window.pageXOffset
-      var top = this.getBoundingClientRect().top + window.pageYOffset
-      var color = colors[Math.floor(Math.random() * colors.length)]
-      hli_num = this.parentNode.id.split("-")[1]
+      var width = this.getBoundingClientRect().width;
+      var height = this.getBoundingClientRect().height;
+      var left = this.getBoundingClientRect().left + window.pageXOffset;
+      var top = this.getBoundingClientRect().top + window.pageYOffset;
+      var color = colors[Math.floor(Math.random() * colors.length)];
+      hli_num = this.parentNode.id.split("-")[1];
 
-      target.style.width = width + "px"
-      target.style.height = height + "px"
-      target.style.left = left + "px"
-      target.style.top = top + "px"
-      target.style.borderColor = color
-      target.style.transform = "none"
+      target.style.width = width + "px";
+      target.style.height = height + "px";
+      target.style.left = left + "px";
+      target.style.top = top + "px";
+      target.style.borderColor = color;
+      target.style.transform = "none";
     }
   }
 
@@ -62,12 +62,12 @@
     for (var i = 0; i < links.length; i++) {
       if ( i != cn ) {
         if (links[i].style.opacity == "1"){
-          leave_i = i
-          links[i].style.opacity = "0.25"
-          target.style.opacity = 0
+          leave_i = i;
+          links[i].style.opacity = "0.25";
+          target.style.opacity = 0;
           target.addEventListener("mouseenter", function(){
-            target.style.opacity = "1"
-            links[leave_i].style.opacity = "1"
+            target.style.opacity = "1";
+            links[leave_i].style.opacity = "1";
           })
         }
       }
@@ -76,72 +76,72 @@
 
   function openMatter3() {
     for (var i = 0; i < links.length; i++) {
-      links[i].style.opacity = "0.25"
+      links[i].style.opacity = "0.25";
     }
-    this.style.opacity = "1"
+    this.style.opacity = "1";
 
     for (var i = 0; i < links.length; i++) {
       if (i == hli_num) {
-        document.getElementById("matter3_" + i).style.display = "block"
-        cn = i
+        document.getElementById("matter3_" + i).style.display = "block";
+        cn = i;
       } 
       else {
-        document.getElementById("matter3_" + i).style.display = "none"
+        document.getElementById("matter3_" + i).style.display = "none";
       }
     }
-    target_copy.style.width = target.style.width
-    target_copy.style.height = target.style.height
-    target_copy.style.left = target.style.left
-    target_copy.style.top = target.style.top
-    target_copy.style.borderColor = target.style.borderColor
-    target_copy.style.transform = target.style.transform
+    target_copy.style.width = target.style.width;
+    target_copy.style.height = target.style.height;
+    target_copy.style.left = target.style.left;
+    target_copy.style.top = target.style.top;
+    target_copy.style.borderColor = target.style.borderColor;
+    target_copy.style.transform = target.style.transform;
 
-    var clickColor = target.style.borderColor
-    var clickColorBackground = rgb2rgba(clickColor)
-    document.getElementsByClassName("suggestion")[0].style.setProperty('--suggestionColor',clickColor)
-    document.getElementsByClassName("suggestion")[0].style.setProperty('--suggestionColorBackground',clickColorBackground)
+    var clickColor = target.style.borderColor;
+    var clickColorBackground = rgb2rgba(clickColor);
+    document.getElementsByClassName("suggestion")[0].style.setProperty('--suggestionColor',clickColor);
+    document.getElementsByClassName("suggestion")[0].style.setProperty('--suggestionColorBackground',clickColorBackground);
 
     if (cn == 3){
-      inputField = document.getElementsByClassName("suggestion-input-field")
+      inputField = document.getElementsByClassName("suggestion-input-field");
       for (var i = 0; i < inputField.length; i++) {
         if (inputField[i].classList.contains("error")){
-          inputField[i].classList.remove("error")
+          inputField[i].classList.remove("error");
         }
       }
-      document.getElementsByClassName("suggestion-comment-hint")[0].innerHTML = ''
+      document.getElementsByClassName("suggestion-comment-hint")[0].innerHTML = '';
     }
   }
   for (var i = 0; i < links.length; i++) {
-    links[i].addEventListener("mouseenter", mouseenterFunc)
-    links[i].addEventListener("click",openMatter3)
+    links[i].addEventListener("mouseenter", mouseenterFunc);
+    links[i].addEventListener("click",openMatter3);
   }
 
   function resizeFunc() {
-    var active = document.querySelector(".personalCenter-menu li.active")
+    var active = document.querySelector(".personalCenter-menu li.active");
 
     if (active) {
-      var left = active.getBoundingClientRect().left + window.pageXOffset
-      var top = active.getBoundingClientRect().top + window.pageYOffset
+      var left = active.getBoundingClientRect().left + window.pageXOffset;
+      var top = active.getBoundingClientRect().top + window.pageYOffset;
 
-      target.style.left = left + "px"
-      target.style.top = top + "px"
+      target.style.left = left + "px";
+      target.style.top = top + "px";
     }
   }
 
   function leaveNav(){
     if (hli_num != cn){
-      links[hli_num].style.opacity = "0.25"
+      links[hli_num].style.opacity = "0.25";
     }
-    target.style.display = "none"
+    target.style.display = "none";
   }
 
   function enterNav(){
-    links[hli_num].style.opacity = "1"
-    target.style.display = "inline"
+    links[hli_num].style.opacity = "1";
+    target.style.display = "inline";
   }
 
-  window.addEventListener("resize", resizeFunc)
-  document.getElementsByClassName("personalCenter-menu")[0].addEventListener("mouseleave",leaveNav)
-  document.getElementsByClassName("personalCenter-menu")[0].addEventListener("mouseenter",enterNav)
+  window.addEventListener("resize", resizeFunc);
+  document.getElementsByClassName("personalCenter-menu")[0].addEventListener("mouseleave",leaveNav);
+  document.getElementsByClassName("personalCenter-menu")[0].addEventListener("mouseenter",enterNav);
   
 })();

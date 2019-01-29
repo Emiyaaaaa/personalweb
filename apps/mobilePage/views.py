da7bc7b106634af73557d84f90b40659cc41ae0e
Emiyaaaaa <2914034404@qq.com>
2019-01-29 18:28:40 +0800

    手机端更新

--- apps/mobilePage/views.py
+++ apps/mobilePage/views.py
@@ -3,7 +3,7 @@ from django.views.generic import View
 
 class MobilePageView(View):
     def get(self,request):
-        return render(request,'404.html')
+        return render(request,'mobilePage.html')
 
     def post(self,request):
-        return render(request,'404.html')
+        return render(request,'mobilePage.html')

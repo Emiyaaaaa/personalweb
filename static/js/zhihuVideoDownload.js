2753fa89d080efefdfc01bd41e93ac4a0d6f25ba
Emiyaaaaa <2914034404@qq.com>
2019-02-07 23:36:07 +0800

    update

--- static/js/zhihuVideoDownload.js
+++ static/js/zhihuVideoDownload.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 	$('#search-button').click(function () {
-		var search_box = document.getElementById('search-box')
-		console.log(search_box.clientHeight)
+		var search_box = document.getElementById('search-box');
+		var search_button = document.getElementById('search-button');
+		var bo_top = search_box.offsetTop;
+		var bu_top = search_button.offsetTop;
 	})
 });

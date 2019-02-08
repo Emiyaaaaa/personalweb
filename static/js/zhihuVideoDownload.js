e7f1ea8462e99bc23aa759a6c50c4f694540e6a1
Emiyaaaaa <2914034404@qq.com>
2019-02-08 15:58:40 +0800

    bug修复

--- static/js/zhihuVideoDownload.js
+++ static/js/zhihuVideoDownload.js
@@ -1,30 +1,32 @@
 $(document).ready(function() {
-	
+
 	$('#search-button').click(function () {
 		var search_box = document.getElementById('search-box');
 		var search_button = document.getElementById('search-button');
-		var bo_top = search_box.offsetTop;
-		var bu_top = search_button.offsetTop;
-		var bo_width = search_box.clientWidth;
-		var bu_width = search_button.clientWidth;
-		var screen_width = document.body.clientWidth;
-		var bo_left = search_box.offsetLeft;
-		var bu_left = search_button.offsetLeft;
-		var bubo_spacing = 2;
-		var a = (screen_width - bo_width - bu_width - bubo_spacing)/2;
-		var set_bo_left = a - bo_left;
-		var set_bu_left = screen_width - a - bu_width - bu_left;
-		var bo_dict = {
-			'top':(-(bo_top-70))+'px',
-			'left':set_bo_left+'px'
-		};
-		var bu_dict = {
-			'top':(-(bu_top-70))+'px',
-			'left':set_bu_left+'px'
-		};
-		setProperty('search-button',bu_dict);
-		setProperty('search-box',bo_dict);
-		
+		if(!search_button.classList.contains("moved")){
+			var bo_top = search_box.offsetTop;
+			var bu_top = search_button.offsetTop;
+			var bo_width = search_box.clientWidth;
+			var bu_width = search_button.clientWidth;
+			var screen_width = document.body.clientWidth;
+			var bo_left = search_box.offsetLeft;
+			var bu_left = search_button.offsetLeft;
+			var bubo_spacing = 2;
+			var a = (screen_width - bo_width - bu_width - bubo_spacing)/2;
+			var set_bo_left = a - bo_left;
+			var set_bu_left = screen_width - a - bu_width - bu_left;
+			var bo_dict = {
+				'top':(-(bo_top-70))+'px',
+				'left':set_bo_left+'px'
+			};
+			var bu_dict = {
+				'top':(-(bu_top-70))+'px',
+				'left':set_bu_left+'px'
+			};
+			search_button.classList.add('moved')
+			setProperty('search-button',bu_dict);
+			setProperty('search-box',bo_dict);
+		}
 	})
 
 	function setProperty(documentObjId,dictObj){

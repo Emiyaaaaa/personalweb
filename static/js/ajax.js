653fa4be0d2460c3fb841a9d159410454699bc60
Emiyaaaaa <2914034404@qq.com>
2019-06-14 19:37:33 +0800

    Update ajax.js

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -136,7 +136,6 @@ $(document).ready(function() {
 	//获取全文	
 	function getMattersContent(href){
 		if (isMobileScreen()) {
-			// $('#left .Wrapper').css('padding-top','74px');//解决上移bug
 			if (document.getElementById('middle').classList.contains('moved')) {
 				document.getElementById("left_list_button").click();//点击后收回列表
 			}

b01cfbc56067b6856ed871fef3c4fa0535adc268
Emiyaaaaa <2914034404@qq.com>
2019-07-13 16:36:27 +0800

    更新了二进制转4,8,16,32的函数

--- static/js/hexconvert.js
+++ static/js/hexconvert.js
@@ -102,9 +102,9 @@ function hexConvert(){
 	var hexToFun = {
 		'2,2':hex2_2(),
 		'2,4':hex2_4(),
-		// '2,8':hex2_8(),
+		'2,8':hex2_8(),
 		'2,10':hex2_10(),
-		// '2,16':hex2_16(),
+		'2,16':hex2_16(),
 		// '2,32':hex2_32(),
 		// '4,2':hex4_2(),
 		// '4,4':hex4_4(),
@@ -139,12 +139,21 @@ function hexConvert(){
 	}
 	hex_array = [hexBeforeConvert,hexAfterConvert];
 	let result = hexToFun[String(hex_array)];
-	result = result.replace(/^0+/g, '').replace(/\.$/g, '');//去除前面的0 和 末尾小数点
+	result = result.replace(/\.$/g, '');//去除末尾小数点
+	if (result.replace(/^0+/g, '') != '') {//去除开始的多余0
+		result = result.replace(/^0+/g, '');
+		if (result.indexOf('.') != -1) {//去除小数末尾多余0
+			result = result.replace(/0+$/g, '');
+		}
+	}
+	if (result == 0) {
+		result = 0;
+	}
 	if (up_down) {
-		document.getElementById('result2').value = result;
+		if (document.getElementById('result1').value != '') {document.getElementById('result2').value = result;}
 	}
 	else{
-		document.getElementById('result1').value = result;
+		if (document.getElementById('result2').value != '') {document.getElementById('result1').value = result;}
 	}
 }
 
@@ -161,16 +170,146 @@ function hex2_4(){
 		'10':'2',
 		'11':'3'
 	}
-	if (value.indexOf('.') == -1 || value.split('.')[1] == ''){//不含小数的情况
-		if (value.length%2 == 1){
-			value = '0' + value;
-		}
-		for (var i = 1; i < value.length; i = i + 2) {
-			result = result + num2_4[value[i-1] + value[i]];
-		}
+	let value_array = value.split('.');
+	value_array[value_array.length] = '';
+	let integer = value_array[0];
+	let decimal = value_array[1];
+	if (integer.length%2 == 1){
+		integer = '0' + integer;
 	}
-	else{
-		console.log(1,value.split('.'))
+	for (var i = 1; i < integer.length; i = i + 2) {
+		result += num2_4[integer[i-1] + integer[i]];
+	}
+	result += '.';
+	if (decimal.length%2 == 1 && decimal.length != 0){
+		decimal = decimal + '0';
+	}
+	for (var i = 1; i < decimal.length; i = i + 2) {
+		result += num2_4[decimal[i-1] + decimal[i]];
+	}
+	return result;
+}
+
+function hex2_8(){
+	let value = String(valueBeforeConvert);
+	let result = '';
+	let zero = '';
+	let num2_8 = {
+		'000':'0',
+		'001':'1',
+		'010':'2',
+		'011':'3',
+		'100':'4',
+		'101':'5',
+		'110':'6',
+		'111':'7'
+	}
+	let value_array = value.split('.');
+	value_array[value_array.length] = '';
+	let integer = value_array[0];
+	let decimal = value_array[1];
+	for (var i = 0; i < 3 - integer.length%3; i++) {
+		zero += '0';
+	}
+	integer = zero + integer;//整数部分前面补0
+	for (var i = 2; i < integer.length; i = i + 3) {
+		result += num2_8[integer[i-2] + integer[i-1] + integer[i]];
+	}
+	result += '.';
+	for (var i = 0; i < 3 - decimal.length%3; i++) {
+		zero += '0';
+	}
+	decimal = decimal + zero;//小数部分后面补0
+	for (var i = 2; i < decimal.length; i = i + 3) {
+		result += num2_8[decimal[i-2] + decimal[i-1] + decimal[i]];
+	}
+	return result;
+}
+
+function hex2_16(){
+	let value = String(valueBeforeConvert);
+	let result = '';
+	let zero = '';
+	let num2_16 = {
+		'0000':'0',
+		'0001':'1',
+		'0010':'2',
+		'0011':'3',
+		'0100':'4',
+		'0101':'5',
+		'0110':'6',
+		'0111':'7',
+		'1000':'8',
+		'1001':'9',
+		'1010':'a',
+		'1011':'b',
+		'1100':'c',
+		'1101':'d',
+		'1110':'e',
+		'1111':'f',
+	}
+	let value_array = value.split('.');
+	value_array[value_array.length] = '';
+	let integer = value_array[0];
+	let decimal = value_array[1];
+	for (var i = 0; i < 4 - integer.length%4; i++) {
+		zero += '0';
+	}
+	integer = zero + integer;//整数部分前面补0
+	for (var i = 3; i < integer.length; i = i + 4) {
+		result += num2_16[integer[i-3] + integer[i-2] + integer[i-1] + integer[i]];
+	}
+	result += '.';
+	for (var i = 0; i < 4 - decimal.length%4; i++) {
+		zero += '0';
+	}
+	decimal = decimal + zero;//小数部分后面补0
+	for (var i = 3; i < decimal.length; i = i + 4) {
+		result += num2_16[decimal[i-3] + decimal[i-2] + decimal[i-1] + decimal[i]];
+	}
+	return result;
+}
+
+function hex2_32(){
+	let value = String(valueBeforeConvert);
+	let result = '';
+	let zero = '';
+	let num2_16 = {
+		'0000':'0',
+		'0001':'1',
+		'0010':'2',
+		'0011':'3',
+		'0100':'4',
+		'0101':'5',
+		'0110':'6',
+		'0111':'7',
+		'1000':'8',
+		'1001':'9',
+		'1010':'a',
+		'1011':'b',
+		'1100':'c',
+		'1101':'d',
+		'1110':'e',
+		'1111':'f',
+	}
+	let value_array = value.split('.');
+	value_array[value_array.length] = '';
+	let integer = value_array[0];
+	let decimal = value_array[1];
+	for (var i = 0; i < 5 - integer.length%5; i++) {
+		zero += '0';
+	}
+	integer = zero + integer;//整数部分前面补0
+	for (var i = 4; i < integer.length; i = i + 5) {
+		result += num2_16[integer[i-4] + integer[i-3] + integer[i-2] + integer[i-1] + integer[i]];
+	}
+	result += '.';
+	for (var i = 0; i < 5 - decimal.length%5; i++) {
+		zero += '0';
+	}
+	decimal = decimal + zero;//小数部分后面补0
+	for (var i = 4; i < decimal.length; i = i + 5) {
+		result += num2_16[decimal[i-4] + decimal[i-3] + decimal[i-2] + decimal[i-1] + decimal[i]];
 	}
 	return result;
 }

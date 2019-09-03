a24a1beca6e7363afd75b5b8399616ece9cbd763
Emiyaaaaa <2914034404@qq.com>
2019-09-03 14:55:36 +0800

    update

--- apps/ngaShadiaoImage/views.py
+++ apps/ngaShadiaoImage/views.py
@@ -54,31 +54,18 @@ class NgaShadiaoImageView(View):
         content_url = request.GET.get('content_url')
         ngaShadiaoImageContentInfo = []
         ngaShadiaoImageContent = NgaShadiaoImageContent.objects.filter(url=content_url).order_by('floor')
+        floor = 1
         title = NgaShadiaoImage.objects.get(url=content_url).title
         for n in ngaShadiaoImageContent:
-            content = self.deal_content(n.content)
+            content = self.deal_content(n.content, floor)
             ngaShadiaoImageContentInfo.append({
                 'content': content,
                 'time': n.time,
-                'floor': n.floor,
+                'floor': floor,
+                'initial_floor': n.floor
             })
+            floor = floor + 1
         return render(request, 'ngaShadiaoImageContent.html', {'ngaShadiaoImageContentInfo': ngaShadiaoImageContentInfo, 'title': title})
-
-    def ajax_get_floor(self, request):
-        content_url = request.GET.get('content_url')
-        floor_num = request.GET.get('floor')
-        ngaShadiaoImageContent = NgaShadiaoImageContent.objects.filter(url=content_url).order_by('floor')
-        if int(floor_num) > int(ngaShadiaoImageContent[0].all_floor_num):
-            return render(request, 'ngaShadiaoImageEnd.html')
-        n = ngaShadiaoImageContent[int(floor_num)]
-        content = self.deal_content(n.content, floor_num)
-        ngaShadiaoImageContentInfo = {
-            'content': content,
-            'time': n.time,
-            'floor': floor_num,
-            'initial_floor':n.floor
-        }
-        return render(request, 'ngaShadiaoImageFloor.html', {'n': ngaShadiaoImageContentInfo})
 
     def get_mzsm(self,request):
         return render(request,'ngaShadiaoImageMzsm.html')
@@ -86,9 +73,12 @@ class NgaShadiaoImageView(View):
     def get_notice(self,request):
         return render(request,'ngaShadiaoImageNotice.html')
 
-    def deal_content(self, content, floor_num=0):
+    def deal_content(self, content, floor_num):
         url = 'https://' + settings.CP_NAME + '.' + settings.CP_DOMAIN + '/'
         content = re.sub('\[s:ac:.*?\]', '', content)
+        if floor_num == 1:
+            content = re.sub('\[img width="(\d+)" height="(\d+)"\]http', lambda x: '<img class="image" width="{}" height="{}" src="{}http'.format(x.group(1), x.group(2), url), content, 5)
+            content = re.sub('\[img width="(\d+)" height="(\d+)"\]./', lambda x: '<img class="image" width="{}" height="{}" src="{}'.format(x.group(1), x.group(2), url), content, 5)
         content = re.sub('\[img width="(\d+)" height="(\d+)"\]http', lambda x: '<img class="image" width="{}" height="{}" data-src="{}http'.format(x.group(1), x.group(2), url), content)
         content = re.sub('\[img width="(\d+)" height="(\d+)"\]./', lambda x: '<img class="image" width="{}" height="{}" data-src="{}'.format(x.group(1), x.group(2), url), content)
         content = content.replace('[/img]', '">')

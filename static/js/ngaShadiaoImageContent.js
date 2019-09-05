b70d192a2112fe6915733260cfb87e05345866aa
Emiyaaaaa <2914034404@qq.com>
2019-09-05 17:55:58 +0800

    实现图片懒加载

--- static/js/ngaShadiaoImageContent.js
+++ static/js/ngaShadiaoImageContent.js
@@ -1,5 +1,5 @@
 (function () {
-	window.onscroll = scrollBottom;
+	window.onscroll = lazyLoad;
 })();
 
 function scrollBottom(){
@@ -36,4 +36,70 @@ function urlSearch(key){
 		urlDict[urlList[i].split('=')[0]] = decodeURIComponent(urlList[i].split('=')[1]);
 	}
 	return urlDict[key];
+}
+
+function lazyLoad(){
+    let clients = window.innerHeight;
+    let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    let wholeHeight = document.body.scrollHeight;
+    let floorEle = document.getElementsByClassName('floor');
+    imgEleList = []
+    for (var i = 0; i < floorEle.length; i++) {
+        if(floorEle[i].offsetTop < clients + scrollTop && floorEle[i].offsetTop + floorEle[i].offsetHeight > clients + scrollTop){
+            loadThisFloor(i, scrollTop, clients);
+            if (floorEle[i].offsetTop > scrollTop) {
+                loadLastFloor(i);
+            }
+            if (floorEle[i].offsetTop + floorEle[i].offsetHeight - clients + scrollTop < 100){
+                loadNextFloor(i);
+            }
+            console.log(i);
+            break;
+            
+        }
+    }
+}
+
+function loadLastFloor(floorNum){
+    let floorEle = document.getElementsByClassName('floor')[floorNum];
+    if (floorEle != undefined) {
+        let imageEle = floorEle.getElementsByTagName('img');
+        for (let i = imageEle.length - 1; i > imageEle.length - 3; i--) {
+            imgEleList.push(imageEle[i]);
+        }
+    }
+}
+
+function loadNextFloor(floorNum){
+    let floorEle = document.getElementsByClassName('floor')[floorNum];
+    if (floorEle != undefined) {
+        let imageEle = floorEle.getElementsByTagName('img');
+        for (let i = 0; i < 2; i++) {
+            imgEleList.push(imageEle[i]);
+        }
+    }
+}
+
+function loadThisFloor(floorNum, scrollTop, clients){
+    let floorEle = document.getElementsByClassName('floor')[floorNum];
+    if (floorEle != undefined) {
+        let imageEle = floorEle.getElementsByTagName('img');
+        for (var i = 0; i < imageEle.length; i++) {
+            if (Math.abs(imageEle[i].offsetTop + floorEle.offsetTop - (scrollTop + clients/2)) < clients/2 + 200) {
+                imgEleList.push(imageEle[i]);
+            }
+        }
+    }
+}
+
+function loadImage(imageList){
+    for (var i = 0; i < imageList.length; i++) {
+        if (imageList[i] != undefined) {
+            let data_src = imageList[i].getAttribute('data-src');
+            if (data_src != null){
+                imageList[i].setAttribute('src', data_src);
+                imageList[i].setAttribute('data-src', null);
+            }
+        }
+    }
 }

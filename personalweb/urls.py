6c303728efd887c71cdd15e04c69d67b88d96687
Emiyaaaaa <2914034404@qq.com>
2020-03-17 13:59:23 +0800

    markdown图片上传功能修复

--- personalweb/urls.py
+++ personalweb/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
@@ -48,7 +48,8 @@ urlpatterns = [
     url(r'^liveAtc$', LiveAtcView.as_view()),
     url(r'^liveAtc/mzsm$', LiveAtcView().get_mzsm),
     url(r'^computerVocabulary', ComputerVocabularyView.as_view()),
-    url(r'api/getWeatherJson', getWeatherJson().getWeatherJson)
+    url(r'api/getWeatherJson', getWeatherJson().getWeatherJson),
+    url(r'^markdown/',include('markdown.urls'))
 ]
 urlpatterns += static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT )
 urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT )
